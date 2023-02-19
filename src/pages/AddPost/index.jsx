ea5fb46b620efdfd4ed97d38406019b361867f6f
Kolesnikov Timur <kolesnikovtimyrweb@gmail.com>
2023-02-19 13:08:35 +0200

    first commit

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import SimpleMDE from 'react-simplemde-editor';
+
+import 'easymde/dist/easymde.min.css';
+import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import axios from '../../axios';
+export const AddPost = () => {
+  const navigate = useNavigate()
+  const isAuth = useSelector(selectIsAuth);
+  const [text, setText] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false)
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const inputFileRef = React.useRef(null)
+  const { id } = useParams()
+
+  const isEditing = Boolean(id)
+
+
+  const handleChangeFile = async (event) => {
+
+    try {
+      const formData = new FormData()
+      formData.append("image", event.target.files[0])
+      const { data } = await axios.post('/upload', formData)
+      setImageUrl(data.url)
+    } catch (err) {
+      console.warn(err)
+      alert('File problem')
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+
+      setLoading(true)
+
+      const fields = {
+        title,
+        text,
+        imageUrl,
+        tags: tags,
+      }
+
+      const { data } = isEditing ? await axios.patch(`/posts/${id}`, fields) : await axios.post('/posts', fields);
+
+      const _id = isEditing ? id : data._id
+      navigate(`/posts/${_id}`)
+    } catch (err) {
+      console.warn(err)
+      alert('Post problem')
+    }
+
+  }
+
+  React.useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`).then(({ data }) => {
+        setTitle(data.title)
+        setImageUrl(data.imageUrl)
+        setTags(data.tags)
+        setText(data.text)
+      }).catch(err => {
+        console.warn(err)
+        alert('Post edit is wrong')
+      })
+    }
+  }, [])
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: '400px',
+      autofocus: true,
+      placeholder: 'Введите текст...',
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    [],
+  );
+
+
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to='/' />
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
+        Загрузить превью
+      </Button>
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
+      {imageUrl && (
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
+      )}
+
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Заголовок статьи..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        fullWidth
+      />
+      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth value={tags}
+        onChange={e => setTags(e.target.value)} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? 'Сохранить' : "Опубликовать"}
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
